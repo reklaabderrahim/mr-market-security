78e794e09a7e8c20c0222143e70c07c2bfa1b764
Rekla Abderrahim <reklaabderrahim@hotmail.com>
2023-05-14 17:59:42 +0200

    before reinstall arch

--- src/main/java/fr/mr_market/mr_market_security/config/SecurityConfig.java
+++ src/main/java/fr/mr_market/mr_market_security/config/SecurityConfig.java
@@ -43,7 +43,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().formLogin().disable().httpBasic().disable().exceptionHandling().authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
+        http.csrf().disable().cors().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .formLogin().disable().httpBasic().disable().exceptionHandling()
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
 //                .authorizeHttpRequests()
 //                    .requestMatchers("/",
 //                            "/error",
@@ -57,7 +60,16 @@ public class SecurityConfig {
 //                            "/**/*.js")
 //                    .permitAll()
 //                    .and()
-                .authorizeHttpRequests().requestMatchers("/api/v1/auth/**", "/api/v1/oauth2/**").permitAll().anyRequest().authenticated().and().authenticationProvider(authenticationProvider).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt).addFilterBefore(new RecaptchaFilter(recaptchaService), UsernamePasswordAuthenticationFilter.class).oauth2Login().authorizationEndpoint().baseUri("/api/v1/oauth2/authorize").authorizationRequestRepository(cookieAuthorizationRequestRepository()).and().redirectionEndpoint().baseUri("/api/v1/oauth2/callback/*").and().userInfoEndpoint().userService(customOAuth2UserService).and().successHandler(oAuth2AuthenticationSuccessHandler).failureHandler(oAuth2AuthenticationFailureHandler).and().logout().logoutUrl("/api/v1/auth/logout").addLogoutHandler(logoutHandler).logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+                .authorizeHttpRequests().requestMatchers("/api/v1/auth/**", "/api/v1/oauth2/**").permitAll().anyRequest().authenticated().and().
+                authenticationProvider(authenticationProvider).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .addFilterBefore(new RecaptchaFilter(recaptchaService), UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login().authorizationEndpoint().baseUri("/api/v1/oauth2/authorize")
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository()).and()
+                .redirectionEndpoint().baseUri("/api/v1/oauth2/callback/*").and().
+                userInfoEndpoint().userService(customOAuth2UserService).and()
+                .successHandler(oAuth2AuthenticationSuccessHandler).failureHandler(oAuth2AuthenticationFailureHandler).and().
+                logout().logoutUrl("/api/v1/auth/logout").addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
 
 
         return http.build();
